1d896ed004a908a75e186d40a8880aec22a81692
carluhsheyn <lkarlashane@gmail.com>
2023-04-01 16:48:20 +0800

    add get user api

--- cafe-services/routes/users.js
+++ cafe-services/routes/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+var auth = require('../services/authentications');
+var checkRole = require('../services/check-role');
 
 router.post('/signup', (req, res) => {
   let user = req.body;
@@ -25,4 +30,34 @@ router.post('/signup', (req, res) => {
   })
 })
 
+router.post('/login', (req, res) => {
+  const user = req.body;
+  query = "select email, password, role, status from users where email = ?";
+  connection.query(query, [user.email], (err, results) => {
+    if(!err) {
+      if(results.length <= 0 || results[0].password !== user.password) {
+        return res.status(401).json({message: "Incorrect username or password."});
+      } else if(results[0].status === 'false') {
+        return res.status(401).json({message: "Wait for admin approval."});
+      } else if(results[0].password === user.password) {
+        const response = {email: results[0].email, role: results[0].role}
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: "8h" });
+        res.status(200).json({token: accessToken});
+      } else {
+        return res.status(400).json({message: "Something went wrong. Please try again later."});
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  })
+})
+
+
+// get user details
+router.get('/get', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+  var query = "select * from users where role = 'user'";
+  connection.query(query, (err, result) => {
+    return err ? res.status(500).json(err) : res.status(200).json(result);
+  })
+})
 module.exports = router;
